92e3f832def20085cf6237701b27ab73fd6606e5
DanielK <rafikrafikrafik@yandex.ru>
2020-03-22 22:53:41 +0300

    Add repr to models

--- myapp/models.py
+++ myapp/models.py
@@ -11,9 +11,15 @@ class ShortUrl(db.Model):
     long_url = db.Column(db.String, nullable=False)
     short_url = db.Column(db.String)
 
+    def __repr__(self):
+        return f"Short URL db entry {self.id} from long {self.long_url}"
+
 
 class Click(db.Model):
     __tablename__ = "clicks"
     short_url = db.Column(db.String, primary_key=True)
     number_of_clicks = db.Column(db.Integer)
     short_url_id = db.Column(db.ForeignKey("shorturls.id"))
+
+    def __repr__(self):
+        return f"This URL {self.short_url} clicked {self.number_of_clicks} times"
