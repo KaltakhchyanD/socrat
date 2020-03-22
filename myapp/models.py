abdacabdf5b87769abab5c6efe5a1086161a2331
DanielK <rafikrafikrafik@yandex.ru>
2020-03-22 15:34:35 +0300

    Add Click model to count clicks on link

--- myapp/models.py
+++ myapp/models.py
@@ -5,7 +5,14 @@ db = SQLAlchemy()
 ma = Marshmallow()
 
 class ShortUrl(db.Model):
-    __tablename_='shorturls'
+    __tablename__='shorturls'
     id = db.Column(db.Integer, primary_key=True)
     long_url=db.Column(db.String, nullable=False)
     short_url=db.Column(db.String)
+
+
+class Click(db.Model):
+    __tablename__='clicks'
+    short_url=db.Column(db.String, primary_key=True)
+    number_of_clicks=db.Column(db.Integer)
+    short_url_id = db.Column(db.ForeignKey("shorturls.id"))
