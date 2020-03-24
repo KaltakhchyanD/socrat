8b819e9085d4dd2b35b30c71cf3354412455c47b
DanielK <rafikrafikrafik@yandex.ru>
2020-03-24 10:38:32 +0300

    Add method that redirects to long url if there is short in DB

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_login import LoginManager, current_user, login_user
 from flask_migrate import Migrate
+import validators
 
 from myapp.config import Config
-from myapp.models import db, ma
+from myapp.models import db, ma, ShortUrl
+from myapp.utils import URLShortener
 from myapp.api.v1.link.views import blueprint as short_link_blueprint
 
 
@@ -22,8 +24,20 @@ def create_app():
         return render_template("index.html")
 
     @app.route("/<string:link>")
-    def ya_page(link):
-        if link == "ya":
-            return redirect("https://yandex.ru")
+    def redirect_to_long_url(link):
+        url_shortener = URLShortener()
+        long_url_id = url_shortener.decode(link)
+        long_url_db_entry = ShortUrl.query.filter_by(id=long_url_id).first()
+        if not long_url_db_entry:
+            abort(404, f"This short link is not valid")
+        long_url = long_url_db_entry.long_url
+        print(f"LOng one - {long_url}")
+
+        # Just in case
+        # Its good practise to check data on entry and exit
+        if not validators.url(long_url):
+            abort(404, f"This long url is not a valid URL!")
+
+        return redirect(long_url)
 
     return app
