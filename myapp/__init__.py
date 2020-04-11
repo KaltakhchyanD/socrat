44edbb4b703894e3fe7875e33df8ad079437b5a5
DanielK <rafikrafikrafik@yandex.ru>
2020-04-11 11:38:45 +0300

    Add 500 error view, just in case

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -103,4 +103,9 @@ def create_app():
     def not_found(error):
         return render_template("404.html"), 404
 
+    @app.errorhandler(500)
+    def internal_error(error):
+        db.session.rollback()
+        return render_template('500.html'), 500
+
     return app
